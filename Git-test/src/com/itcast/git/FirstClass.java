dbcbd7151942773547d10c686844529a1e8b3c92
liangzhi <lz245350678@qq.com>
2018-11-10 17:22:33 +0800

    第二次提交

---
+++ Git-test/src/com/itcast/git/FirstClass.java
@@ -0,0 +1,13 @@
+package com.itcast.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello 1");
+        System.out.println("hello 1");
+        System.out.println("hello 1");
+        System.out.println("hello 1");
+        System.out.println("hello 1");
+        System.out.println("hello 1");
+        System.out.println("hello 1");
+    }
+}
